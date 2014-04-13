95e501bdac1e280b775e11fd12fa2c9cc011f860
Stakey-Zh <starkey86@163.com>
2014-04-13 21:50:16 +0800

    test

--- shicheng/src/com/stake/shicheng/module/login/Activity_Login.java
+++ shicheng/src/com/stake/shicheng/module/login/Activity_Login.java
@@ -1,6 +1,7 @@
 package com.stake.shicheng.module.login;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import com.stake.shicheng.R;
 import com.stake.shicheng.base.BaseActivity;
 
@@ -11,6 +12,8 @@ import com.stake.shicheng.base.BaseActivity;
  * Time: 11:25
  */
 public class Activity_Login extends BaseActivity {
+    private EditText mEdt_user;
+    private EditText mEdt_psw;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
